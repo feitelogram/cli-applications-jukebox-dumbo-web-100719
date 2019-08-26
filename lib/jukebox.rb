286bd9f9c2e2293ba0fa3af962ef79e623e6b788
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-26 14:48:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -60,3 +60,20 @@ def play (songs)
     end
   end
   end
+  
+  require "pry"
+
+array = [1,2,3,4]
+
+def average (array)
+  total = 0 
+  average = 0 
+  i= 0
+  array.length.times do
+    total += array[i]
+    i+=1
+  end
+ average = total / array.length
+ average
+ binding.pry
+end
